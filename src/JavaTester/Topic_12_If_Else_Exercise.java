2a1693b68a2c285de2ba65f6ce65ce84a44637c6
MinhThuAutomation <MinhThu@automation>
2022-08-26 15:49:33 +0700

    Topic_12_If_Else_Exercise

---
+++ src/JavaTester/Topic_12_If_Else_Exercise.java
@@ -0,0 +1,89 @@
+package JavaTester;
+
+import java.util.Scanner;
+
+class Topic_12_If_Else_Exercise {
+public static void main(String[] args) {
+    Scanner scanner = new Scanner (System.in);
+//    int n;
+//    n = scanner.nextInt();
+//    if (n%2==0) {
+//    	System.out.println( n + " la so chan");
+//    }
+//    else {
+//    	System.out.println(n + " la so le");
+//    }
+//    
+//    System.out.println("Exercise 2");
+//    
+//    int a,b;
+//    a = scanner.nextInt();
+//    b= scanner.nextInt();
+//    if(a>=b) {
+//    	System.out.println(a + " lon hon hoac bang " + b);
+//    }
+//    else {
+//    	System.out.println(a + " be hon " + b);
+//    	
+//    }
+//    	
+    System.out.println("Exercise 3");
+    	
+    
+    	String person1 = scanner.nextLine();
+    	String person2= scanner.nextLine();
+    	 if (person1.equals(person2)) {
+    		 System.out.println("cung ten");
+    	 }
+    	 else {
+    		 System.out.println( "khac ten");
+    	 }
+////    	 
+//    System.out.println("Exercise 4");
+//    int x, y, z;
+//    x= scanner.nextInt();
+//    y=scanner.nextInt();
+//    z=scanner.nextInt();
+//    if (x>=y && x>=z) {
+//    	System.out.println("x");
+//    }
+//     else if (y>=z){
+//    	 System.out.println("y");
+//     }
+//     else {
+//    	 System.out.println("z");
+//     }
+    
+//    System.out.println("Exercise 5");
+//    
+//    int number;
+//    number= scanner.nextInt();
+//    if (number>=10 && number<=100) {
+//    	System.out.println(number+ " belong to [10,100]");
+//    }
+//    else {
+//    	System.out.println(number+ "do not belong to [10,100]");
+//    }
+//    
+//    
+//    System.out.println("Exercise 6");
+//   float score;
+//   score=scanner.nextFloat();
+//   if (score<5) {
+//	   System.out.println("Diem D");
+//	   
+//   }
+//   else if (score>=5 && score<7.5) {
+//	   System.out.println("Diem C");
+//   }
+//   else if(score>=7.5 && score<8.5) {
+//	   System.out.println("Diem B");
+//   }
+//   else if(score>=8.5 && score<=10){
+//	   System.out.println("Diem A");
+//   }
+//   else {
+//	   System.out.println("Please iput again!");
+//   }
+}
+}
